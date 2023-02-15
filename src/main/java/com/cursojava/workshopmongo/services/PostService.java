92e761e01d5cb910d74247a5aabfafbbe833d82d
Bruno Dias <brunoadias1996@gmail.com>
2023-02-15 08:40:58 -0300

    obtendo um post por id

---
+++ src/main/java/com/cursojava/workshopmongo/services/PostService.java
@@ -0,0 +1,21 @@
+package com.cursojava.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursojava.workshopmongo.domain.Post;
+import com.cursojava.workshopmongo.repository.PostRepository;
+import com.cursojava.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto Não Encontrado"));	
+	}
+}
